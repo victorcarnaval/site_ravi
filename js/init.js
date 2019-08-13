e8cd827b10e2900145f52ea093b681710bf10e18
Victor Carnaval <victorlopescarnaval@gmail.com>
2019-08-12 21:59:17 -0300

    Correção visual do countdown

--- js/init.js
+++ js/init.js
@@ -7,55 +7,27 @@ document.addEventListener('DOMContentLoaded', function () {
   const parallaxElem = document.querySelectorAll('.parallax');
   const parallaxInstance = M.Parallax.init(parallaxElem);
 
-  // CountDown
-  const countDownDate = new Date('Nov 9, 2019 17:30:00 GMT-0300').getTime();
+  // Countdown
+  const eventDate = new Date('Nov 9, 2019 17:30:00 GMT-0300').getTime();
 
-  const autoCountDown = setInterval(function () {
+  const timer = setInterval(function () {
 
-    const now = new Date().getTime(),
-      distance = countDownDate - now,
-      countDown = {};
+    let now = new Date().getTime();
+    let distance = eventDate - now;
 
-    countDown.dia = Math.floor(distance / (1000 * 60 * 60 * 24));
-    countDown.hora = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    countDown.minuto = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    countDown.segundo = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let secs = Math.floor((distance % (1000 * 60)) / 1000);
 
-    printCountDown(countDown);
+    document.getElementById('timer-days').innerHTML = ('0' + days).slice(-2) + '&nbsp;<span>dia(s)</span>';
+    document.getElementById('timer-hours').innerHTML = ('0' + hours).slice(-2) + '&nbsp;<span>hora(s)</span>';
+    document.getElementById('timer-mins').innerHTML = ('0' + mins).slice(-2) + '&nbsp;<span>minuto(s)</span>';
+    document.getElementById('timer-secs').innerHTML = ('0' + secs).slice(-2) + '&nbsp;<span>segundo(s)</span>';
 
-    if (distance < 0) {
-      clearInterval(autoCountDown);
+    if (distance <= 0) {
+      clearInterval(timer);
     }
   }, 1000);
 
-  function printCountDown(countDown) {
-    const items = document.querySelectorAll('.countdown > li');
-
-    let index = 0;
-    for (property in countDown) {
-      const value = countDown[property];
-      const li = items[index];
-      const num = li.childNodes[0];
-      const lbl = li.childNodes[1];
-
-      num.textContent = value.toString().length === 1 ? '0' + value.toString() : value;
-      lbl.textContent = (value > 1) ? property + 's' : property;
-
-      index++;
-    }
-  }
-
-  function beautyNumber(number, label) {
-    let num = number.toString();
-
-    if (num.length === 1) {
-      num = '0' + num;
-    }
-
-    if (parseInt(num) > 1 || parseInt(num) === 0) {
-      label = label + 's'; // Ex.: dia para dias
-    }
-
-    return { number: num, label: label };
-  }
 });
